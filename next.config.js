79d984f00de80ed5d00f718db2f93ac66178ccf7
evanmeli1 <evanmeli2003@gmail.com>
2025-08-22 19:49:42 -0700

    enable custom domain asset prefix

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const nextConfig = withPWA({
   },
   
   // Add CDN configuration
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://bayarea-resource.com' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://bayarea-resources.com' : '',
   trailingSlash: true,
   compress: true,
   
